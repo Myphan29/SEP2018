0761f6b70eca02dd204cb5c38da7dbf5df2c32de
KiritoVo <KiritoVo@KIRITOVO-PC>
2018-04-05 22:39:50 +0700

    Giao diện Log in

--- SEP_FingerPrint/Controllers/HomeController.cs
+++ SEP_FingerPrint/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace SEP_FingerPrint.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
