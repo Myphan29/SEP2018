9ba46c00194247399490a5ebe159e1739ed69468
KiritoVo <KiritoVo@KIRITOVO-PC>
2018-04-09 18:26:58 +0700

    LoginModels và somthing

---
+++ SEP_FingerPrint/Models/LoginModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEP_FingerPrint.Models
+{
+    public class LoginModels
+    {
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public string RememberMe { get; set; }
+    }
+}
