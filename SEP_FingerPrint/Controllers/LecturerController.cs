c2e81383b756d1cd89ffb8a3385d74dcf6919c9e
Myphan29 <tramy290997@gmail.com>
2018-04-10 21:01:34 +0700

    load all database into schedule and fix bug layout

--- SEP_FingerPrint/Controllers/LecturerController.cs
+++ SEP_FingerPrint/Controllers/LecturerController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SEP_FingerPrint.Models;
 
 namespace SEP_FingerPrint.Controllers
 {
     public class LecturerController : Controller
     {
+        private SepEntities db = new SepEntities();
         // GET: Lecturer
         public ActionResult Index()
         {
@@ -16,6 +18,17 @@ namespace SEP_FingerPrint.Controllers
         public ActionResult Schedule()
         {
             return View();
+        }
+       public ActionResult FindAll()
+        {
+            return Json(db.BuoiHocs.AsEnumerable().Select(e => new {
+                id = Convert.ToInt32(e.MBH),
+                title = e.Phong,
+                description = e.Phong,
+                start = e.Ngay.Value.ToString("yyyy/MM/dd")+"T"+e.GioBatDau.Value.ToString(),
+                end= e.Ngay.Value.ToString("yyyy/MM/dd") + "T" + e.GioKetThuc.Value.ToString(),
+
+            }).ToList(), JsonRequestBehavior.AllowGet);
         }
         public ActionResult Attendance()
         {
