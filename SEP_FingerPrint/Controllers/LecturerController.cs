3158eb4e3dd8b957e87f5338561a118f031e8612
Minh Minh <tnnm.vtn@gmail.com>
2018-04-14 00:55:45 +0700

    Connect DB and Load data into table

--- SEP_FingerPrint/Controllers/LecturerController.cs
+++ SEP_FingerPrint/Controllers/LecturerController.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using SEP_FingerPrint.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Linq.Dynamic;
+using System.Data.Entity;
 
 namespace SEP_FingerPrint.Controllers
 {
     public class LecturerController : Controller
     {
+        private SepEntities db = new SepEntities();
         // GET: Lecturer
         public ActionResult Index()
         {
@@ -20,6 +24,35 @@ namespace SEP_FingerPrint.Controllers
         public ActionResult Attendance()
         {
             return View();
+        }
+
+        public ActionResult LoadData()
+        {
+            List<DiemDanh> _list = new List<DiemDanh>();
+            try
+            {
+                _list = db.DiemDanhs.ToList();
+                var result = from c in _list
+                             //where c.MBH == "1"
+                             select new[]
+                             {
+                                 Convert.ToString( c.ID ),
+                                 Convert.ToString( c.MSV ),
+                                 Convert.ToString( c.MBH ),
+                                 Convert.ToString( c.Ngay ),
+                                 Convert.ToString( c.Gio ),
+                                 Convert.ToString( c.TrangThai ),
+                             };
+                return Json(new { aaData = result }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                //ErrorLogers.ErrorLog(ex);
+                return Json(new
+                {
+                    aaData = new List<string[]> { }
+                }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
