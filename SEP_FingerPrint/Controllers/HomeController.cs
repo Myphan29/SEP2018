02ef9b0ff477faf3ed885bc122776a3d1bc252b7
KiritoVo <KiritoVo@KIRITOVO-PC>
2018-04-16 17:09:17 +0700

    Sửa lỗi Merge

--- SEP_FingerPrint/Controllers/HomeController.cs
+++ SEP_FingerPrint/Controllers/HomeController.cs
@@ -26,15 +26,20 @@ namespace SEP_FingerPrint.Controllers
             if (ModelState.IsValid)
             {
                 var result = Login(model.UserName, MD5Hash(model.Password));
-                if (result==true)
+                if (result == true)
                 {
+                    var userSession = new UserLogin();
+                    Session.Add("USER_SESSION", userSession);
                     var user = GetByID(model.UserName);
+                    var pass = MD5Hash(model.Password);
+                    var userdetail = db.TaiKhoans.Where(x => x.TenTK == model.UserName && x.matkhau == pass).FirstOrDefault();
+                    Session["ID"] = userdetail.ID;
+                    Session["MSGV"] = userdetail.TenTK;
+                    Session["Role"] = userdetail.Vaitro;
+                    Session["TenGV"] = db.GiangViens.ToList().FirstOrDefault(p => p.IDTaiKhoan == userdetail.ID).HoTen;
 
-                    userSession.UserID = user.ID;
-                    userSession.UserName = user.TenTK;
-                    userSession.Role = user.Vaitro;
 
-                    return RedirectToAction("Schedule", "Lecturer");
+                    return RedirectToAction("Course", "Lecturer");
                 }
                 else
                 {
