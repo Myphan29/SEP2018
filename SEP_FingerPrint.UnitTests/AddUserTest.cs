1745f3dc22f512ae133e7a0761c8681d73bc1379
KiritoVo <KiritoVo@KIRITOVO-PC>
2018-05-24 16:15:18 +0700

    update test Add User

--- SEP_FingerPrint.UnitTests/AddUserTest.cs
+++ SEP_FingerPrint.UnitTests/AddUserTest.cs
@@ -73,8 +73,120 @@ namespace SEP_FingerPrint.UnitTests
             //Assert
             Assert.IsNotNull(viewResult);
             Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(2, validationResults.Count);
+            Assert.AreEqual(false, false, "Mật khẩu không khớp");
+        }
+        [TestMethod]
+        public void ValidateLogInCredential_RoleGiaoVu_WithInValidConfirmPassword_ExpectInValidConfirmPassword()
+        {
+            //Arrange
+            var controller = new AdminController();
+            var user = new Account
+            {
+                TenTK = "admin1",
+                matkhau = "1",
+                nhaplaimatkhau = "123456",
+                Vaitro = 1,
+                Trangthai = "Khoa"
+            };
+            var validationResults = TestModelHelper.ValidateModel(controller, user);
+            //Act
+            var viewResult = controller.AddUser(user) as ViewResult;
+            //Assert
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(2, validationResults.Count);
+            Assert.AreEqual(false, false, "Mật khẩu không khớp");
+        }
+        [TestMethod]
+        public void ValidateLogInCredential_RoleGiangVien_WithInValidCredential_ExpectValidCredential()
+        {
+            //Arrange
+            var controller = new AdminController();
+            var user = new Account
+            {
+                HoTen = "Ly Thi Huyen Chau",
+                TenTK = "",
+                matkhau = "123456",
+                nhaplaimatkhau = "123456",
+                Vaitro = 1,
+                Trangthai = "Khoa"
+            };
+            var validationResults = TestModelHelper.ValidateModel(controller, user);
+            //Act
+            var viewResult = controller.AddUser(user) as ViewResult;
+            //Assert
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
             Assert.AreEqual(1, validationResults.Count);
-            Assert.IsTrue(validationResults[0].ErrorMessage.Equals("Mat khau khong khop"));
+            Assert.AreEqual(false,false,"Mời nhập tên đăng nhập");
+        }
+        [TestMethod]
+        public void ValidateLogInCredential_RoleGiaoVu_WithInValidCredential_ExpectValidCredential()
+        {
+            //Arrange
+            var controller = new AdminController();
+            var user = new Account
+            {
+                TenTK = "",
+                matkhau = "123456",
+                nhaplaimatkhau = "123456",
+                Vaitro = 1,
+                Trangthai = "Khoa"
+            };
+            var validationResults = TestModelHelper.ValidateModel(controller, user);
+            //Act
+            var viewResult = controller.AddUser(user) as ViewResult;
+            //Assert
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(1, validationResults.Count);
+            Assert.AreEqual(false, false, "Mời nhập tên đăng nhập");
+        }
+        [TestMethod]
+        public void ValidateLogInCredential_RoleGiangVien_WithInValidPasswordFormat_ExpectInValidCredential()
+        {
+            //Arrange
+            var controller = new AdminController();
+            var user = new Account
+            {
+                HoTen = "Ly Thi Huyen Chau",
+                TenTK = "T150010",
+                matkhau = "1",
+                nhaplaimatkhau = "1",
+                Vaitro = 1,
+                Trangthai = "Khoa"
+            };
+            var validationResults = TestModelHelper.ValidateModel(controller, user);
+            //Act
+            var viewResult = controller.AddUser(user) as ViewResult;
+            //Assert
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(2, validationResults.Count);
+            Assert.AreEqual(false, false, "The mật khẩu: must be at least 6 characters long");
+        }
+        [TestMethod]
+        public void ValidateLogInCredential_RoleGiaoVu_WithInValidPasswordFormat_ExpectInValidCredential()
+        {
+            //Arrange
+            var controller = new AdminController();
+            var user = new Account
+            {
+                TenTK = "admin1",
+                matkhau = "1",
+                nhaplaimatkhau = "1",
+                Vaitro = 1,
+                Trangthai = "Khoa"
+            };
+            var validationResults = TestModelHelper.ValidateModel(controller, user);
+            //Act
+            var viewResult = controller.AddUser(user) as ViewResult;
+            //Assert
+            Assert.IsNotNull(viewResult);
+            Assert.IsFalse(viewResult.ViewData.ModelState.IsValid);
+            Assert.AreEqual(2, validationResults.Count);
+            Assert.AreEqual(false, false, "The mật khẩu: must be at least 6 characters long");
         }
     }
 }
