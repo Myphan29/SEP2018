6551420d60ce0775bf6d25f356088145a5540cce
KiritoVo <KiritoVo@KIRITOVO-PC>
2018-05-29 15:11:52 +0700

    Change Password

---
+++ SEP_FingerPrint.AcceptanceTests/Features/ChangePassword.feature
@@ -0,0 +1,26 @@
+﻿Feature: ChangePassword
+	In order to change password
+	As a user
+	I want to change password
+Background: 
+	Given User is at Change Password page
+
+Scenario: Change password successfully
+	When I enter Old Password = 123456 and New Password = 123456789 and Confirm New Password =123456789
+	Then the result should be display successful message Đổi mật khẩu thành công
+
+Scenario: Change password unsuccess with wrong Old password
+	When i enter Old Password = 1234567 and New Password = 123456789 and Confirm New Password =123456789
+	Then the result should be display error message Mật khẩu cũ sai
+
+Scenario: Change password unsuccess with wrong password condition
+	When i enter Old Password = 123456 and New Password = 1 and Confirm New Password =1
+	Then the result should be display error message Đổi mật khẩu không thành công with reason At least 6 characters long
+
+Scenario: Change password unsuccess with wrong password confirm
+	When i enter Old Password = 123456 and New Password = 123456789 and Confirm New Password =1
+	Then the result should be display error message Đổi mật khẩu không thành công with reason The new password and confirmation password do not match
+
+Scenario: Change password unsuccess with same password on Old password and New password
+	When i enter Old Password = 123456 and New Password = 123456 and Confirm New Password =123456
+	Then the result should be display error message Mật khẩu mới không được giống mật khẩu cũ
---
+++ SEP_FingerPrint.SeleniumTests/ChangePasswordSteps.cs
@@ -0,0 +1,102 @@
+﻿using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SEP_FingerPrint.SeleniumTests
+{
+    [Binding]
+    public sealed class ChangePasswordSteps
+    {
+        public IWebDriver driver = new ChromeDriver();
+        [Given(@"User is at Change Password page")]
+        public void GivenUserIsAtTheChangePasswordPage()
+        {
+            driver.Navigate().GoToUrl("http://localhost:49354/Lecturer/ChangePassword");
+        }
+        [When(@"User enter Old Password = 123456 And New Password = 123456789 And Confirm New Password =123456789")]
+        public void WhenUserEnterOldPasswordAndNewPasswordAndConfirmNewPassword()
+        {
+            driver.FindElement(By.XPath("//*[@id='OldPassword']")).SendKeys("123456");
+            driver.FindElement(By.XPath("//*[@id='NewPassword']")).SendKeys("123456789");
+            driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("123456789");
+            driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[4]/div/input")).Click();
+        }
+        [When(@"User enter Old Password = 1234567 ")]
+        public void WhenUserEnterWrongOldPassword()
+        {
+            driver.FindElement(By.XPath("//*[@id='OldPassword']")).SendKeys("1234567");
+            driver.FindElement(By.XPath("//*[@id='NewPassword']")).SendKeys("123456789");
+            driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("123456789");
+            driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[4]/div/input")).Click();
+        }
+
+        [When(@"User enter New Password = 1 and Confirm New Password = 1 ")]
+        public void WhenUserEnterWrongPasswordCondition()
+        {
+            driver.FindElement(By.XPath("//*[@id='OldPassword']")).SendKeys("123456");
+            driver.FindElement(By.XPath("//*[@id='NewPassword']")).SendKeys("1");
+            driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("1");
+            driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[4]/div/input")).Click();
+        }
+
+        [When(@"User enter Confirm New Password = 1234567")]
+        public void WhenUserEnterWrongConfirmNewPassword()
+        {
+            driver.FindElement(By.XPath("//*[@id='OldPassword']")).SendKeys("123456");
+            driver.FindElement(By.XPath("//*[@id='NewPassword']")).SendKeys("123456789");
+            driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("1234567");
+            driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[4]/div/input")).Click();
+        }
+
+        [When(@"User enter Old Password = 123456 And New Password = 123456 ")]
+        public void WhenUserEnterSamePasswordOnOldPasswordAndNewPassword()
+        {
+            driver.FindElement(By.XPath("//*[@id='OldPassword']")).SendKeys("123456");
+            driver.FindElement(By.XPath("//*[@id='NewPassword']")).SendKeys("123456");
+            driver.FindElement(By.XPath("//*[@id='ConfirmPassword']")).SendKeys("123456");
+            driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[4]/div/input")).Click();
+        }
+        [Then(@"I can see success message Đổi mật khẩu thành công")]
+        public void ThenICanSeeSuccessMessage()
+        {
+
+            string expectedResult = "Đổi mật khẩu thành công";
+            string actualResult = driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[5]/div")).Text;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Then(@"Appear message ""(.*)"" because wrong Old Password")]
+        public void ThenAppearMessageBecauseWrongOldPassword(string p0)
+        {
+
+            string expectedResult = p0;
+            string actualResult = driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[5]/div")).Text;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Then(@"Appear message ""(.*)"" because wrong password condition")]
+        public void ThenAppearMessageBecauseWrongPasswordCondition(string p0)
+        {
+
+            string expectedResult = p0;
+            string actualResult = driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[5]/div")).Text;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Then(@"Appear message ""(.*)"" because wrong Confirm Password")]
+        public void ThenAppearMessageBecauseWrongConfirmNewPassword(string p0)
+        {
+
+            string expectedResult = p0;
+            string actualResult = driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[5]/div")).Text;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Then(@"Appear message ""(.*)"" because Old and New same password")]
+        public void ThenAppearMessageBecauseOldAndNewSamePassword(string p0)
+        {
+
+            string expectedResult = p0;
+            string actualResult = driver.FindElement(By.XPath("//*[@id='main - wrapper']/div[3]/div[2]/form/div[5]/div")).Text;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
