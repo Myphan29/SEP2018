b30f5746ebcd5e920745bd8573abb46309ccec43
Long Nguyen <nguyenthanhlong7997@gmail.com>
2018-07-01 13:36:36 +0700

    Unit test - Login and log out, Reset Password, Change Status

---
+++ SEP_FingerPrint.UnitTests/ChangeStatus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SEP_FingerPrint.Controllers;
+using SEP_FingerPrint.Models;
+using SEP_FingerPrint.UnitTests.Support;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+
+namespace SEP_FingerPrint.UnitTests
+{
+    [TestClass]
+    public class ChangeStatus
+    {
+        [TestMethod]
+        public void ValidateAdminCanChangeStatusOfAccountUser()
+        {
+            var controller = new AdminController();
+            var user = new TaiKhoan().ID;
+            user = 37;
+
+            var validationResults = TestModelHelper.ValidateModel(controller, user);
+
+            
+           
+            var result = controller.changeStt(user);
+            var stt = "Disable";
+            
+            var viewResult = controller.Edit() as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+            Assert.AreEqual(result, stt);
+           
+            Assert.AreEqual(0, validationResults.Count);
+        }
+        
+    }
+}
